20c8be53da6220144d217da303d80e84c88731c8
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-03 18:13:35 +0200

    feat(jsMain): added ToggleButton adapter

---
+++ src/jsMain/kotlin/react/adapters/reactBootstrap/buttons/ToggleButton.kt
@@ -0,0 +1,32 @@
+@file:JsModule("react-bootstrap/ToggleButton")
+@file:JsNonModule
+
+package react.adapters.reactBootstrap.buttons
+
+import react.ComponentClass
+import react.Props
+
+/**
+ * React Bootstrap ToggleButton adapter
+ * @see <a href="https://react-bootstrap.github.io/components/buttons/">
+ *     react-bootstrap - buttons</a>
+ */
+
+@JsName("default")
+external val ToggleButton: ComponentClass<ToggleButtonProps>
+
+/**
+ * Props used to customize the ToggleButton.
+ */
+external interface ToggleButtonProps : Props {
+
+    /**
+     * id props, required.
+     */
+    var id: String
+
+    /**
+     * value props.
+     */
+    var value: Any
+}
