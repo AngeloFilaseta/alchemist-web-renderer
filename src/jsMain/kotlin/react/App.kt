42a0c0dbf8b0b3ee554d4225a1434841e1716285
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-04 12:20:35 +0200

    feat(jsMain): added store to application, added logic to navbar

--- src/jsMain/kotlin/react/App.kt
+++ src/jsMain/kotlin/react/App.kt
@@ -1,6 +1,15 @@
 package react
 
+import org.reduxkotlin.Store
+import org.reduxkotlin.createStore
 import react.components.AppNavbar
+import state.AppState
+import state.rootReducer
+
+/**
+ * The application store. The object that brings reducers and actions together.
+ */
+val store: Store<AppState> = createStore(::rootReducer, AppState())
 
 /**
  * The App to render.
--- src/jsMain/kotlin/react/components/AppNavbar.kt
+++ src/jsMain/kotlin/react/components/AppNavbar.kt
@@ -6,7 +6,13 @@ import react.adapters.reactBootstrap.buttons.ToggleButton
 import react.adapters.reactBootstrap.buttons.ToggleButtonGroup
 import react.adapters.reactBootstrap.navbar.Navbar
 import react.adapters.reactBootstrap.navbar.NavbarBrand
+import react.store
+import state.actions.RenderMode
+import state.actions.SetRenderMode
 
+/**
+ * The application navbar.
+ */
 val AppNavbar: FC<Props> = FC {
     Navbar {
         bg = "dark"
@@ -18,22 +24,22 @@ val AppNavbar: FC<Props> = FC {
         ToggleButtonGroup {
             name = "toggle-render-mode"
             onChange = { value ->
-                console.log("onChange: $value")
+                store.dispatch(SetRenderMode(value as RenderMode))
             }
             defaultValue = "auto"
             ToggleButton {
                 id = "client-button"
-                value = "client"
+                value = RenderMode.valueOf("CLIENT")
                 +"Client"
             }
             ToggleButton {
                 id = "auto-button"
-                value = "auto"
+                value = RenderMode.valueOf("AUTO")
                 +"Auto"
             }
             ToggleButton {
                 id = "server-button"
-                value = "server"
+                value = RenderMode.valueOf("SERVER")
                 +"Server"
             }
         }
