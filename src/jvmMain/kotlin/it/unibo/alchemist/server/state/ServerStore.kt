735ea1472b4ca5907a9c95be972368865c5f958e
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-04 19:04:31 +0200

    feat(jvmMain): added ServerStore object

---
+++ src/jvmMain/kotlin/it/unibo/alchemist/server/state/ServerStore.kt
@@ -0,0 +1,14 @@
+package it.unibo.alchemist.server.state
+
+import org.reduxkotlin.Store
+import org.reduxkotlin.createThreadSafeStore
+
+/**
+ * The store of the application. It is stored using a singleton pattern.
+ */
+object ServerStore {
+    /**
+     * The redux store of the server.
+     */
+    val store: Store<ServerState<Any, Nothing>> = createThreadSafeStore(::rootReducer, ServerState(null))
+}
