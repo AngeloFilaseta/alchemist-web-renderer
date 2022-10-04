3caeb80e0fa711d2428bdff5edf45455d2f708a9
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-04 15:08:30 +0200

    feat(commonMain): added SimulationAction

---
+++ src/commonMain/kotlin/it/unibo/alchemist/model/SimulationAction.kt
@@ -0,0 +1,17 @@
+package it.unibo.alchemist.model
+
+/**
+ * Actions that can be performed on a simulation.
+ */
+enum class SimulationAction {
+
+    /**
+     * The action used to start the Simulation.
+     */
+    PLAY,
+
+    /**
+     * The action used to pause the Simulation.
+     */
+    PAUSE
+}
