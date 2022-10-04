115263410c6476b0414271599502db144e5bad42
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-04 14:56:41 +0200

    feat(commonMain): added Environment paths

---
+++ src/commonMain/kotlin/it/unibo/alchemist/model/Environment.kt
@@ -0,0 +1,25 @@
+package it.unibo.alchemist.model
+
+/**
+ * The Environment to render. This is a Surrogate class that will be used instead of the original Alchemist Environment.
+ */
+@Suppress("UtilityClassWithPublicConstructor") // TODO Remove in the future
+class Environment {
+
+    /**
+     * The available routes for the environment retrieval process.
+     */
+    companion object {
+        private const val path: String = "/environment"
+
+        /**
+         * Route used if the client will execute the render computation.
+         */
+        const val clientModePath: String = "$path/client"
+
+        /**
+         * Route used if the server will execute the render computation.
+         */
+        const val serverModePath: String = "$path/server"
+    }
+}
