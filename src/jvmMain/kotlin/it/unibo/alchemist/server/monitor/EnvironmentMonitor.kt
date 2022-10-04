c2b3e702e58ed2f069134900024ed2245ed0fe4e
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-04 15:35:34 +0200

    docs(jvmMain): added missing documentation in EnvironmentMonitor

--- src/jvmMain/kotlin/it/unibo/alchemist/server/monitor/EnvironmentMonitor.kt
+++ src/jvmMain/kotlin/it/unibo/alchemist/server/monitor/EnvironmentMonitor.kt
@@ -14,15 +14,28 @@ import it.unibo.alchemist.model.interfaces.Time
  */
 class EnvironmentMonitor<T, P : Position<out P>> : OutputMonitor<T, P> {
 
+    /**
+     * Every time the environment changes, set it as the current environment.
+     */
     override fun stepDone(environment: Environment<T, P>, reaction: Actionable<T>?, time: Time, step: Long) {
         println("Step done") // TODO, update the global Environment state
     }
 
+    /**
+     * Call the stepDone method setting time and step to 0.
+     */
     override fun initialized(environment: Environment<T, P>) {
         this.stepDone(environment, null, Time.ZERO, 0L)
     }
 
+    /**
+     * Call the stepDone method setting the reaction to null.
+     */
     override fun finished(environment: Environment<T, P>, time: Time, step: Long) {
         this.stepDone(environment, null, time, step)
+    }
+
+    companion object {
+        private const val serialVersionUID: Long = 1L
     }
 }
