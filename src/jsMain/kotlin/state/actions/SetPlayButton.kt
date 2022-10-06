3a21dd19da935671afd7ed4b8fb68d1b34090ce2
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-06 15:46:34 +0200

    feat(jsMain): added setPlayButton action

---
+++ src/jsMain/kotlin/state/actions/SetPlayButton.kt
@@ -0,0 +1,9 @@
+package state.actions
+
+import it.unibo.alchemist.model.SimulationAction
+
+/**
+ * Redux action to set the Simulation state of the application and switch between Play and pause.
+ * @param simulationAction the new simulation action to set.
+ */
+data class SetPlayButton(val simulationAction: SimulationAction)
