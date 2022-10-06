c06dab85dfade4ade665a7f0ec68d5175dae1233
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-06 16:00:41 +0200

    refactor(jsMain): Moved navbar button group to components package

--- src/jsMain/kotlin/react/components/AppNavbar.kt
+++ src/jsMain/kotlin/react/components/AppNavbar.kt
@@ -1,14 +1,9 @@
 package react.components
 
-import it.unibo.alchemist.model.RenderMode
 import react.FC
 import react.Props
-import react.adapters.reactBootstrap.buttons.ToggleButton
-import react.adapters.reactBootstrap.buttons.ToggleButtonGroup
 import react.adapters.reactBootstrap.navbar.Navbar
 import react.adapters.reactBootstrap.navbar.NavbarBrand
-import react.store
-import state.actions.SetRenderMode
 
 /**
  * The application navbar.
@@ -20,29 +15,7 @@ val AppNavbar: FC<Props> = FC {
         NavbarBrand {
             +"Alchemist Web Renderer"
         }
-
-        ToggleButtonGroup {
-            name = "toggle-render-mode"
-            onChange = { value ->
-                store.dispatch(SetRenderMode(value as RenderMode))
-            }
-            defaultValue = RenderMode.valueOf("AUTO")
-            ToggleButton {
-                id = "client-button"
-                value = RenderMode.valueOf("CLIENT")
-                +"Client"
-            }
-            ToggleButton {
-                id = "auto-button"
-                value = RenderMode.valueOf("AUTO")
-                +"Auto"
-            }
-            ToggleButton {
-                id = "server-button"
-                value = RenderMode.valueOf("SERVER")
-                +"Server"
-            }
-        }
+        RenderModeButtons()
         PlayButton()
     }
 }
