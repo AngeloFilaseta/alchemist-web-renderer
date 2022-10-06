b20ca2cc4b53b3c72cc1008615f5320d970dc1e3
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-06 15:52:17 +0200

    feat(jsMain): added PlayButton to AppState

--- src/jsMain/kotlin/state/AppState.kt
+++ src/jsMain/kotlin/state/AppState.kt
@@ -1,6 +1,8 @@
 package state
 
 import it.unibo.alchemist.model.RenderMode
+import it.unibo.alchemist.model.SimulationAction
+import state.reducers.playButtonReducer
 import state.reducers.renderModeReducer
 
 /**
@@ -10,12 +12,18 @@ data class AppState(
     /**
      * The render mode of the application. It can be either client, server or auto. It is set to auto by default.
      */
-    val renderMode: RenderMode = RenderMode.AUTO
+    val renderMode: RenderMode = RenderMode.AUTO,
+
+    /**
+     * The action to execute on the simulation.
+     */
+    val playButton: SimulationAction = SimulationAction.PAUSE
 )
 
 /**
  * Root reducer of the application. Uses all the other reducers.
  */
 fun rootReducer(state: AppState, action: Any): AppState = AppState(
-    renderMode = renderModeReducer(state.renderMode, action)
+    renderMode = renderModeReducer(state.renderMode, action),
+    playButton = playButtonReducer(state.playButton, action)
 )
