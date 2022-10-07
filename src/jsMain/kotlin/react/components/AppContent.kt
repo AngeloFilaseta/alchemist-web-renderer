4211beff6ce933c9a20fc51c36b8b1d80de96ef0
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-07 12:08:04 +0200

    feat(jsMain): added AppContent with simple string print

---
+++ src/jsMain/kotlin/react/components/AppContent.kt
@@ -0,0 +1,43 @@
+package react.components
+
+import io.ktor.util.InternalAPI
+import io.ktor.utils.io.readUTF8Line
+import it.unibo.alchemist.model.SimulationAction
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.js.timers.setInterval
+import react.FC
+import react.Props
+import react.dom.html.ReactHTML.p
+import react.logic.hwAutoStrategy
+import react.logic.retrieveEnvironment
+import react.store
+import react.useState
+import kotlin.time.Duration
+
+private val scope = MainScope()
+
+/**
+ * The application main content section.
+ */
+@OptIn(InternalAPI::class) // TODO remove in the future, used for testing because of httpResponse.content
+val AppContent: FC<Props> = FC {
+    var environment: String by useState("")
+
+    suspend fun setEnvironment() {
+        val httpResponse = retrieveEnvironment(store.state.renderMode, hwAutoStrategy)
+        environment = "[${httpResponse.status}] ${httpResponse.content.readUTF8Line()}"
+    }
+
+    setInterval(Duration.parse("3s")) {
+        if (store.state.playButton == SimulationAction.PLAY) {
+            scope.launch {
+                setEnvironment()
+            }
+        }
+    }
+
+    p {
+        +environment
+    }
+}
