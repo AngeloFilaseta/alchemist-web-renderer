d57b95daf0e29e92e6719f4e51e335f0d8072594
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-10 14:57:59 +0200

    refactor(commonMain): added serialName to NodeSurrogate

--- src/commonMain/kotlin/it/unibo/alchemist/model/surrogate/NodeSurrogate.kt
+++ src/commonMain/kotlin/it/unibo/alchemist/model/surrogate/NodeSurrogate.kt
@@ -1,8 +1,21 @@
 package it.unibo.alchemist.model.surrogate
 
+import it.unibo.alchemist.model.surrogate.NodeSurrogate.Companion.serialName
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
 /**
  * Surrogate class for the [it.unibo.alchemist.model.interfaces.Node] interface.
  * @param id the id of the node.
  * @param <T> the type of concentration.
  */
-data class NodeSurrogate<T>(val id: Int)
+@Serializable
+@SerialName(serialName)
+data class NodeSurrogate<T>(val id: Int) {
+    companion object {
+        /**
+         * The serial name used by the class.
+         */
+        const val serialName: String = "Node"
+    }
+}
