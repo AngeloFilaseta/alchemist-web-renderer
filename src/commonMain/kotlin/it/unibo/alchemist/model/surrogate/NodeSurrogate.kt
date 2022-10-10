8202960e4ed5fab93ec3d18d32adb14949042ee2
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-10 14:42:31 +0200

    feat(commonMain): added NodeSurrogate Class

---
+++ src/commonMain/kotlin/it/unibo/alchemist/model/surrogate/NodeSurrogate.kt
@@ -0,0 +1,8 @@
+package it.unibo.alchemist.model.surrogate
+
+/**
+ * Surrogate class for the [it.unibo.alchemist.model.interfaces.Node] interface.
+ * @param id the id of the node.
+ * @param <T> the type of concentration.
+ */
+data class NodeSurrogate<T>(val id: Int)
