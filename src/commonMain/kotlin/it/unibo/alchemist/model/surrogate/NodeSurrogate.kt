db8963d6669c773e215f897d71c07718470992b8
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-10 15:24:06 +0200

    feat(commonMain): added contents to Node

--- src/commonMain/kotlin/it/unibo/alchemist/model/surrogate/NodeSurrogate.kt
+++ src/commonMain/kotlin/it/unibo/alchemist/model/surrogate/NodeSurrogate.kt
@@ -7,11 +7,15 @@ import kotlinx.serialization.Serializable
 /**
  * Surrogate class for the [it.unibo.alchemist.model.interfaces.Node] interface.
  * @param id the id of the node.
+ * @param contents the mapping between molecules and concentrations.
  * @param <T> the type of concentration.
  */
 @Serializable
 @SerialName(serialName)
-data class NodeSurrogate<T>(val id: Int) {
+data class NodeSurrogate<T>(
+    val id: Int,
+    val contents: Map<MoleculeSurrogate, T>
+) {
     companion object {
         /**
          * The serial name used by the class.
