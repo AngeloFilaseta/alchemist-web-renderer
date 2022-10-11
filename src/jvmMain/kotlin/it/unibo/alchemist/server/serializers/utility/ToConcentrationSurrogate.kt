34d0f8c3bd13b4632775b631c8653b516ebd9969
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-11 17:00:30 +0200

    feat(jvmMain): added ToConcentrationSurrogate object, sapere implemented

---
+++ src/jvmMain/kotlin/it/unibo/alchemist/server/serializers/utility/ToConcentrationSurrogate.kt
@@ -0,0 +1,19 @@
+package it.unibo.alchemist.server.serializers.utility
+
+import it.unibo.alchemist.model.interfaces.ILsaMolecule
+import it.unibo.alchemist.model.surrogate.concentrations.sapere.ILsaMoleculeSurrogate
+
+/**
+ * A set of functions to map the concentrations to the corresponding surrogate classes.
+ */
+object ToConcentrationSurrogate {
+
+    /**
+     * A function that maps a list of [it.unibo.alchemist.model.interfaces.ILsaMolecule] a list of
+     * [it.unibo.alchemist.model.surrogate.concentrations.sapere.ILsaMoleculeSurrogate]. This Concentration is used
+     * by the SAPERE Incarnation.
+     */
+    val toSapereConcentrationSurrogate: (List<ILsaMolecule>) -> List<ILsaMoleculeSurrogate> = { concentration ->
+        concentration.map { ILsaMoleculeSurrogate(it.name, listOf()) }
+    }
+}
