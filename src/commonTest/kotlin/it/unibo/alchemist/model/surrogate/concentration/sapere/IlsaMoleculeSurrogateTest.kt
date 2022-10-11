0587bfc4417a630c779f52e391b3de1c87e54f14
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-11 15:57:35 +0200

    test(commonMain): added test class for ILsaMoleculeSurrogate

---
+++ src/commonTest/kotlin/it/unibo/alchemist/model/surrogate/concentration/sapere/IlsaMoleculeSurrogateTest.kt
@@ -0,0 +1,18 @@
+package it.unibo.alchemist.model.surrogate.concentration.sapere
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import it.unibo.alchemist.model.surrogate.concentrations.sapere.ILsaMoleculeSurrogate
+
+class IlsaMoleculeSurrogateTest : StringSpec({
+
+    val surrogate = ILsaMoleculeSurrogate("test", listOf(1, 2))
+
+    "Surrogate IlsaMolecule should have the correct name" {
+        surrogate.name shouldBe "test"
+    }
+
+    "Surrogate IlsaMolecule should have the correct expressions" {
+        surrogate.expressions shouldBe listOf(1, 2)
+    }
+})
