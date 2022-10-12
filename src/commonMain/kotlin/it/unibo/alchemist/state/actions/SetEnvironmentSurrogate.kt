d651e91294eae8eb8ad481e89c0bd8ed73bd5c64
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-12 14:42:51 +0200

    docs(commonMain): added missing parameters in SetEnvironmentSurrogate

--- src/commonMain/kotlin/it/unibo/alchemist/state/actions/SetEnvironmentSurrogate.kt
+++ src/commonMain/kotlin/it/unibo/alchemist/state/actions/SetEnvironmentSurrogate.kt
@@ -5,6 +5,8 @@ import it.unibo.alchemist.model.surrogate.PositionSurrogate
 
 /**
  * Redux action to set the EnvironmentSurrogate of the application.
+ * @param <TS> type of the concentration surrogate.
+ * @param <PS> type of the position surrogate.
  * @param environmentSurrogate the new environment surrogate.
  */
 data class SetEnvironmentSurrogate<TS, PS : PositionSurrogate>(val environmentSurrogate: EnvironmentSurrogate<TS, PS>)
