5cea8b7fdef84943f5b23cb37260d64bccb7a239
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-10-14 17:07:32 +0200

    test(commonTest): added position test for NodeSurrogate

--- src/commonTest/kotlin/it/unibo/alchemist/model/surrogate/NodeSurrogateTest.kt
+++ src/commonTest/kotlin/it/unibo/alchemist/model/surrogate/NodeSurrogateTest.kt
@@ -10,7 +10,9 @@ class NodeSurrogateTest : StringSpec({
 
     val mapping = mapOf("test-0" to 0, "test-1" to 1)
 
-    val surrogateNode = NodeSurrogate(29, mapping)
+    val nodePosition = Position2DSurrogate(5.6, 1.2)
+
+    val surrogateNode = NodeSurrogate(29, mapping, nodePosition)
 
     "SurrogateNode should have the correct id" {
         surrogateNode.id shouldBe 29
@@ -20,6 +22,10 @@ class NodeSurrogateTest : StringSpec({
         surrogateNode.contents["test-0"] shouldBe 0
         surrogateNode.contents["test-1"] shouldBe 1
         surrogateNode.contents.size shouldBe 2
+    }
+
+    "SurrogateNode should have a position" {
+        surrogateNode.position shouldBe nodePosition
     }
 
     "SurrogateNode should be serialized correctly" {
